ff6f980566c293a8800a47db8084950dcccd5df7
Firebird492 <gavinbuier@outlook.com>
2024-11-15 16:15:59 -0500

    update models_test to match the autogen tool
    This added a check for every field that as well as checks arrays full content rather that just part of it.

--- BackEnd/model_test.js
+++ BackEnd/model_test.js
@@ -1,229 +1,261 @@
-
-const { MenuItem, Order, Account, PickupLocation } = require("./models.js");
+const { MenuItem, Order, Account, PickupLocation } = require("./models.js")
 const assert = require("assert");
+
+
+function areArraysEqual(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  
+  return true;
+}
+
 
 function testCreateMenuItem(){
     const exampleItem = {  
         _id : 1,
-        name : "example",
-        price : 23.4,
-        allergen : "vegetarian",
-        description : "leaves",
-        image : "www.example.com",
-        category: "Main Dish" 
+        name : "example1", 
+        price : 1.2, 
+        allergen : "example1", 
+        description : "example1", 
+        image : "example1", 
+        active : true, 
+        category : "example1"
     };
-    const menuItem = new MenuItem(exampleItem);
+    const item = new MenuItem(exampleItem);
     
-    assert.strictEqual(menuItem._id, exampleItem._id, "id should equal exampleItem.id");
-    assert.strictEqual(menuItem.name, exampleItem.name, "name should equal exampleItem.name");
-    assert.strictEqual(menuItem.price, exampleItem.price, "price should equal exampleItem.price");
-    assert.strictEqual(menuItem.allergen, exampleItem.allergen, "allergen should equal exampleItem.allergen");
-    assert.strictEqual(menuItem.description, exampleItem.description, "description should equal exampleItem.description");
-    assert.strictEqual(menuItem.category, exampleItem.category, "category should equal exampleItem.category");
+    assert.strictEqual(item._id, exampleItem._id, "id should equal exampleItem.id");
+    assert.strictEqual(item.name, exampleItem.name, "name should equal exampleItem.name");
+    assert.strictEqual(item.price, exampleItem.price, "price should equal exampleItem.price");
+    assert.strictEqual(item.allergen, exampleItem.allergen, "allergen should equal exampleItem.allergen");
+    assert.strictEqual(item.description, exampleItem.description, "description should equal exampleItem.description");
+    assert.strictEqual(item.image, exampleItem.image, "image should equal exampleItem.image");
+    assert.strictEqual(item.active, exampleItem.active, "active should equal exampleItem.active");
+    assert.strictEqual(item.category, exampleItem.category, "category should equal exampleItem.category");
 }
 
 
 function testCreateEmptyMenuItem(){
-    const menuItem = new MenuItem({});
+    const item = new MenuItem({});
     
-    assert.strictEqual(menuItem.name, "", "name should be empty");
-    assert.strictEqual(menuItem.price, -1, "price should equal be -1");
-    assert.strictEqual(menuItem.allergen, "", "allergen should be empty");
-    assert.strictEqual(menuItem.description, "", "description should be empty");
-    assert.strictEqual(menuItem.active, true, "The menu item should be active");
+    assert.strictEqual(item.name, "", 'name should be ""');
+    assert.strictEqual(item.price, -1, 'price should be -1');
+    assert.strictEqual(item.allergen, "", 'allergen should be ""');
+    assert.strictEqual(item.description, "", 'description should be ""');
+    assert.strictEqual(item.image, "", 'image should be ""');
+    assert.strictEqual(item.active, true, 'active should be true');
+    assert.strictEqual(item.category, "Main", 'category should be "Main"');
 }
 
 
 function testCreateMenuItemWithNullInput(){
     const exampleItem = {  
         _id : null,
-        name : null,
-        price : null,
-        allergen : null,
-        description : null,
-        image : null 
+        name : null, 
+        price : null, 
+        allergen : null, 
+        description : null, 
+        image : null, 
+        active : null, 
+        category : null
     };
-    const menuItem = new MenuItem(exampleItem);
+    const item = new MenuItem(exampleItem);
     
-    assert.strictEqual(menuItem._id, null, "id should be null");
-    assert.strictEqual(menuItem.name, "", "name should be empty");
-    assert.strictEqual(menuItem.price, -1, "price should be -1");
-    assert.strictEqual(menuItem.allergen, "", "allergen should be empty");
-    assert.strictEqual(menuItem.description, "", "description should be empty");
+    assert.strictEqual(item._id, null, "id should be null");
+    assert.strictEqual(item.name, "", 'name should be ""');
+    assert.strictEqual(item.price, -1, 'price should be -1');
+    assert.strictEqual(item.allergen, "", 'allergen should be ""');
+    assert.strictEqual(item.description, "", 'description should be ""');
+    assert.strictEqual(item.image, "", 'image should be ""');
+    assert.strictEqual(item.active, true, 'active should be true');
+    assert.strictEqual(item.category, "Main", 'category should be "Main"');
 }
 
 
 function testCreateOrder(){
-    const exampleOrder = {  
+    const exampleItem = {  
         _id : 1,
-        accountId : 2,
-        orderTime : "2023-09-24T15:30:45Z",
-        pickupLocation : 3,
-        items : [2,4,6,1],
-        costOfItems : 34,
-        tip : 3.4,
-        completed : "2023-09-24T16:30:45Z",
+        accountId : "example1", 
+        orderTime : "example1", 
+        pickupLocation : "example1", 
+        items : ['arrayVal1', 'arrayVal2', 'arrayVal3'], 
+        costOfItems : 1.2, 
+        tip : 1.2, 
+        completed : "example1"
     };
-    const order = new Order(exampleOrder);
+    const item = new Order(exampleItem);
     
-    assert.strictEqual(order._id, exampleOrder._id, "id should equal exampleOrder.id");
-    assert.strictEqual(order.accountId, exampleOrder.accountId, "accountId should equal exampleOrder.accountId");
-    assert.strictEqual(order.orderTime, exampleOrder.orderTime, "orderTime should equal exampleOrder.orderTime");
-    assert.strictEqual(order.pickupLocation, exampleOrder.pickupLocation, "pickupLocation should equal exampleOrder.pickupLocation");
-    assert.strictEqual(order.items, exampleOrder.items, "items should equal exampleOrder.items");
-    assert.strictEqual(order.costOfItems, exampleOrder.costOfItems, "costOfItems should equal exampleOrder.costOfItems");
-    assert.strictEqual(order.tip, exampleOrder.tip, "tip should equal exampleOrder.tip");
-    assert.strictEqual(order.completed, exampleOrder.completed, "completed should equal exampleOrder.completed");
+    assert.strictEqual(item._id, exampleItem._id, "id should equal exampleItem.id");
+    assert.strictEqual(item.accountId, exampleItem.accountId, "accountId should equal exampleItem.accountId");
+    assert.strictEqual(item.orderTime, exampleItem.orderTime, "orderTime should equal exampleItem.orderTime");
+    assert.strictEqual(item.pickupLocation, exampleItem.pickupLocation, "pickupLocation should equal exampleItem.pickupLocation");
+    assert.strictEqual(item.costOfItems, exampleItem.costOfItems, "costOfItems should equal exampleItem.costOfItems");
+    assert.strictEqual(item.tip, exampleItem.tip, "tip should equal exampleItem.tip");
+    assert.strictEqual(item.completed, exampleItem.completed, "completed should equal exampleItem.completed");
+    assert.strictEqual(areArraysEqual(item.items, exampleItem.items), true ); 
 }
 
 
 function testCreateEmptyOrder(){
-    const order = new Order({});
+    const item = new Order({});
     
-    assert.strictEqual(order._id, null, "id should be null");
-    assert.strictEqual(order.accountId, null, "accountId should be null");
-    assert.strictEqual(order.orderTime,  "", "orderTime should be empty");
-    assert.strictEqual(order.pickupLocation, -1, "pickupLocation should equal -1");
-    assert.strictEqual(order.items.length, 0, "items should be empty");
-    assert.strictEqual(order.costOfItems, -1, "costOfItems should equal -1");
-    assert.strictEqual(order.tip, -1, "tip should equal -1");
-    assert.strictEqual(order.completed, "", "completed should be empty");
+    assert.strictEqual(item.accountId, null, 'accountId should be null');
+    assert.strictEqual(item.orderTime, "", 'orderTime should be ""');
+    assert.strictEqual(item.pickupLocation, null, 'pickupLocation should be null');
+    assert.strictEqual(item.costOfItems, -1, 'costOfItems should be -1');
+    assert.strictEqual(item.tip, -1, 'tip should be -1');
+    assert.strictEqual(item.completed, "", 'completed should be ""');
+    assert.strictEqual(areArraysEqual(item.items, []), true ); 
 }
 
 
 function testCreateOrderWithNullInput(){
-    const exampleOrder = {  
+    const exampleItem = {  
         _id : null,
-        accountId : null,
-        orderTime : null,
-        pickupLocation : null,
-        items : null,
-        costOfItems : null,
-        tip : null,
-        completed : null,
+        accountId : null, 
+        orderTime : null, 
+        pickupLocation : null, 
+        items : null, 
+        costOfItems : null, 
+        tip : null, 
+        completed : null
     };
-    const order = new Order(exampleOrder);
+    const item = new Order(exampleItem);
     
-    assert.strictEqual(order._id, null, "id should be null");
-    assert.strictEqual(order.accountId, null, "accountId should be null");
-    assert.strictEqual(order.orderTime,  "", "orderTime should be empty");
-    assert.strictEqual(order.pickupLocation, -1, "pickupLocation should equal -1");
-    assert.strictEqual(order.items.length, 0, "items should be empty");
-    assert.strictEqual(order.costOfItems, -1, "costOfItems should equal -1");
-    assert.strictEqual(order.tip, -1, "tip should equal -1");
-    assert.strictEqual(order.completed, "", "completed should be empty");
+    assert.strictEqual(item._id, null, "id should be null");
+    assert.strictEqual(item.accountId, null, 'accountId should be null');
+    assert.strictEqual(item.orderTime, "", 'orderTime should be ""');
+    assert.strictEqual(item.pickupLocation, null, 'pickupLocation should be null');
+    assert.strictEqual(item.costOfItems, -1, 'costOfItems should be -1');
+    assert.strictEqual(item.tip, -1, 'tip should be -1');
+    assert.strictEqual(item.completed, "", 'completed should be ""');
+    assert.strictEqual(areArraysEqual(item.items, []), true ); 
 }
 
 
 function testCreateAccount(){
-    const exampleAccount = {  
+    const exampleItem = {  
         _id : 1,
-        name : "the tester",
-        email : "theTester@test.com",
-        phone : "122-333-4444",
+        name : "example1", 
+        email : "example1", 
+        phone : "example1", 
+        password : "example1", 
         accessLevel : 1, 
-        cart : [1, 2, 3, 4]
+        cart : ['arrayVal1', 'arrayVal2', 'arrayVal3']
     };
-    const account = new Account(exampleAccount);
+    const item = new Account(exampleItem);
     
-    assert.strictEqual(account._id, exampleAccount._id, "id should equal exampleAccount.id");
-    assert.strictEqual(account.name, exampleAccount.name, "name should equal exampleAccount.name");
-    assert.strictEqual(account.email, exampleAccount.email, "email should equal exampleAccount.email");
-    assert.strictEqual(account.phone, exampleAccount.phone, "phone should equal exampleAccount.phone");
-    assert.strictEqual(account.accessLevel, exampleAccount.accessLevel, "accessLevel should equal exampleAccount.accessLevel");
-    assert.strictEqual(account.cart, exampleAccount.cart, "cart should equal exampleAccount.cart");
-
+    assert.strictEqual(item._id, exampleItem._id, "id should equal exampleItem.id");
+    assert.strictEqual(item.name, exampleItem.name, "name should equal exampleItem.name");
+    assert.strictEqual(item.email, exampleItem.email, "email should equal exampleItem.email");
+    assert.strictEqual(item.phone, exampleItem.phone, "phone should equal exampleItem.phone");
+    assert.strictEqual(item.password, exampleItem.password, "password should equal exampleItem.password");
+    assert.strictEqual(item.accessLevel, exampleItem.accessLevel, "accessLevel should equal exampleItem.accessLevel");
+    assert.strictEqual(areArraysEqual(item.cart, exampleItem.cart), true ); 
 }
 
 
 function testCreateEmptyAccount(){
-    const account = new Account({});
+    const item = new Account({});
     
-    assert.strictEqual(account._id, null, "id should be null");
-    assert.strictEqual(account.name, "", "name should be empty");
-    assert.strictEqual(account.email, "", "email should be empty");
-    assert.strictEqual(account.phone, "", "phone should be empty");
-    assert.strictEqual(account.accessLevel, -1, "accessLevel should equal -1");
-    assert.strictEqual(account.cart.length, 0, "cart should be empty");
+    assert.strictEqual(item.name, "", 'name should be ""');
+    assert.strictEqual(item.email, "", 'email should be ""');
+    assert.strictEqual(item.phone, "", 'phone should be ""');
+    assert.strictEqual(item.password, "", 'password should be ""');
+    assert.strictEqual(item.accessLevel, -1, 'accessLevel should be -1');
+    assert.strictEqual(areArraysEqual(item.cart, []), true ); 
 }
 
 
 function testCreateAccountWithNullInput(){
-    const exampleAccount = {  
+    const exampleItem = {  
         _id : null,
-        name : null,
-        email : null,
-        phone : null,
+        name : null, 
+        email : null, 
+        phone : null, 
+        password : null, 
         accessLevel : null, 
         cart : null
     };
-    const account = new Account(exampleAccount);
+    const item = new Account(exampleItem);
     
-    assert.strictEqual(account._id, null, "id should be null");
-    assert.strictEqual(account.name, "", "name should be empty");
-    assert.strictEqual(account.email, "", "email should be empty");
-    assert.strictEqual(account.phone, "", "phone should be empty");
-    assert.strictEqual(account.accessLevel, -1, "accessLevel should equal -1");
-    assert.strictEqual(account.cart.length, 0, "cart should be empty");
-
+    assert.strictEqual(item._id, null, "id should be null");
+    assert.strictEqual(item.name, "", 'name should be ""');
+    assert.strictEqual(item.email, "", 'email should be ""');
+    assert.strictEqual(item.phone, "", 'phone should be ""');
+    assert.strictEqual(item.password, "", 'password should be ""');
+    assert.strictEqual(item.accessLevel, -1, 'accessLevel should be -1');
+    assert.strictEqual(areArraysEqual(item.cart, []), true ); 
 }
 
 
 function testCreatePickupLocation(){
-    const exampleLocation = {  
+    const exampleItem = {  
         _id : 1,
-        address : "123 RPI Rd.",
-        contactInfo : "123-456-7890",
-        name : "RPI union"
+        address : "example1", 
+        contactInfo : "example1", 
+        name : "example1", 
+        active : true, 
+        pickupTime : "example1"
     };
-    const location = new PickupLocation(exampleLocation);
+    const item = new PickupLocation(exampleItem);
     
-    assert.strictEqual(location._id, exampleLocation._id, "id should equal exampleLocation.id");
-    assert.strictEqual(location.address, exampleLocation.address, "address should equal exampleLocation.address");
-    assert.strictEqual(location.contactInfo, exampleLocation.contactInfo, "contactInfo should equal exampleLocation.contactInfo");
-    assert.strictEqual(location.name, exampleLocation.name, "name should equal exampleLocation.name");
-
+    assert.strictEqual(item._id, exampleItem._id, "id should equal exampleItem.id");
+    assert.strictEqual(item.address, exampleItem.address, "address should equal exampleItem.address");
+    assert.strictEqual(item.contactInfo, exampleItem.contactInfo, "contactInfo should equal exampleItem.contactInfo");
+    assert.strictEqual(item.name, exampleItem.name, "name should equal exampleItem.name");
+    assert.strictEqual(item.active, exampleItem.active, "active should equal exampleItem.active");
+    assert.strictEqual(item.pickupTime, exampleItem.pickupTime, "pickupTime should equal exampleItem.pickupTime");
 }
 
 
 function testCreateEmptyPickupLocation(){
-    const location = new PickupLocation({});
-    assert.strictEqual(location.address, "", "address should be empty");
-    assert.strictEqual(location.contactInfo, "", "contactInfo should be empty");
-    assert.strictEqual(location.name, "", "name should be empty");
-
+    const item = new PickupLocation({});
+    
+    assert.strictEqual(item.address, "", 'address should be ""');
+    assert.strictEqual(item.contactInfo, "", 'contactInfo should be ""');
+    assert.strictEqual(item.name, "", 'name should be ""');
+    assert.strictEqual(item.active, false, 'active should be false');
+    assert.strictEqual(item.pickupTime, "", 'pickupTime should be ""');
 }
 
 
 function testCreatePickupLocationWithNullInput(){
-    const exampleLocation = {  
+    const exampleItem = {  
         _id : null,
-        address : null,
-        contactInfo : null,
-        name : null
+        address : null, 
+        contactInfo : null, 
+        name : null, 
+        active : null, 
+        pickupTime : null
     };
-    const location = new PickupLocation(exampleLocation);
+    const item = new PickupLocation(exampleItem);
     
-    assert.strictEqual(location._id, null, "id should be null");
-    assert.strictEqual(location.address, "", "address should be empty");
-    assert.strictEqual(location.contactInfo, "", "contactInfo should be empty");
-    assert.strictEqual(location.name, "", "name should be empty");
-
+    assert.strictEqual(item._id, null, "id should be null");
+    assert.strictEqual(item.address, "", 'address should be ""');
+    assert.strictEqual(item.contactInfo, "", 'contactInfo should be ""');
+    assert.strictEqual(item.name, "", 'name should be ""');
+    assert.strictEqual(item.active, false, 'active should be false');
+    assert.strictEqual(item.pickupTime, "", 'pickupTime should be ""');
 }
 
 
-function runTests(){
+
+
+function runTests(){ 
     testCreateMenuItem();
-    testCreateOrder();
-    testCreateAccount();
-    testCreatePickupLocation();
     testCreateEmptyMenuItem();
-    testCreateEmptyOrder();
-    testCreateEmptyAccount();
-    testCreateEmptyPickupLocation();
     testCreateMenuItemWithNullInput();
+    testCreateOrder();
+    testCreateEmptyOrder();
     testCreateOrderWithNullInput();
+    testCreateAccount();
+    testCreateEmptyAccount();
     testCreateAccountWithNullInput();
+    testCreatePickupLocation();
+    testCreateEmptyPickupLocation();
     testCreatePickupLocationWithNullInput();
     console.log("tests passed");
 }
