fe634ccea3498aab0c6a61d70a69a0d87ffc225c
Firebird492 <gavinbuier@outlook.com>
2025-03-14 17:13:09 -0400

    update tests to not use a mock renderer

--- FrontEnd/app/__tests__/HomeScreen-test.tsx
+++ FrontEnd/app/__tests__/HomeScreen-test.tsx
@@ -5,48 +5,30 @@ import HomeScreen from '@/app/(tabs)/index';
 
 const Tab = createBottomTabNavigator();
 
-const MockNavigator = () => (
-  <NavigationContainer>
-    <Tab.Navigator>
-      <Tab.Screen name="homescreen" component={HomeScreen} />
-    </Tab.Navigator>
-  </NavigationContainer>
-);
-
 describe('HomeScreen', () => {
   it('should render HomeScreen', () => {
     // Render the HomeScreen component wrapped in the navigator
-    render(<MockNavigator />);
+    const { getByPlaceholderText, getByText, getByTestId } = render(<HomeScreen />);
     
-    let linkElement = screen.getByText("Welcome to Thunder Mountain Curry!");
-    expect(linkElement).toBeOnTheScreen();
+    expect(getByText("Welcome to Thunder Mountain Curry!")).toBeTruthy();
     
-    linkElement = screen.getByText("Our Story");
-    expect(linkElement).toBeOnTheScreen();
+    expect(getByText("Our Story")).toBeTruthy();
     
-    linkElement = screen.getByText("From our beginnings out of a hot dog cart to the RPI Student Union, the Troy Waterfront Farmer's Market, and our Pandemic Pop-Ups, TMC has never wavered from our mission - to bring the Troy, NY community a mouth-watering culinary adventure straight from the streets. Our new journey takes us back to our roots as a true street food experience. Follow us on Instagram and FaceBook to see where we're serving today!");
-    expect(linkElement).toBeOnTheScreen();
+    expect(getByText("From our beginnings out of a hot dog cart to the RPI Student Union, the Troy Waterfront Farmer's Market, and our Pandemic Pop-Ups, TMC has never wavered from our mission - to bring the Troy, NY community a mouth-watering culinary adventure straight from the streets. Our new journey takes us back to our roots as a true street food experience. Follow us on Instagram and FaceBook to see where we're serving today!")).toBeTruthy();
     
-    linkElement = screen.getByText("Our Food");
-    expect(linkElement).toBeOnTheScreen();
+    expect(getByText("Our Food")).toBeTruthy();
     
-    linkElement = screen.getByText("Thunder Mountain Curry focuses on quality ingredients and authentic Pan-Asian recipes with our own twist.\nTMC is a unique street food experience for those seeking a delicious and satisfying culinary adventure.");
-    expect(linkElement).toBeOnTheScreen();
+    expect(getByText("Thunder Mountain Curry focuses on quality ingredients and authentic Pan-Asian recipes with our own twist.\nTMC is a unique street food experience for those seeking a delicious and satisfying culinary adventure.")).toBeTruthy();
     
-    linkElement = screen.getByText("Visit Us");
-    expect(linkElement).toBeOnTheScreen();
+    expect(getByText("Visit Us")).toBeTruthy();
     
-    linkElement = screen.getByText("Now at the Troy Waterfront Farmers Market and in front of the RPI Student Union - follow us to find out when!");
-    expect(linkElement).toBeOnTheScreen();
+    expect(getByText("Now at the Troy Waterfront Farmers Market and in front of the RPI Student Union - follow us to find out when!")).toBeTruthy();
      
-    let image = screen.getByTestId("tmc-image");
-    expect(image).toBeOnTheScreen();
+    expect(getByTestId("tmc-image")).toBeTruthy();
     
-    image = screen.getByTestId("insta-image");
-    expect(image).toBeOnTheScreen();
+    expect(getByTestId("insta-image")).toBeTruthy();
     
-    image = screen.getByTestId("facebook-image");
-    expect(image).toBeOnTheScreen();
+    expect(getByTestId("facebook-image")).toBeTruthy();
     
   });
 });
