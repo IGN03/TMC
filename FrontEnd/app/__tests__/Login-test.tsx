b5eea3610a4f1a4e271bc917b1e75558fa568029
Firebird492 <gavinbuier@outlook.com>
2025-04-04 16:54:43 -0400

    add tests for input fields missing or having bad info

--- FrontEnd/app/__tests__/Login-test.tsx
+++ FrontEnd/app/__tests__/Login-test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { render, fireEvent } from '@testing-library/react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import LoginScreen from '@/app/(tabs)/login';
@@ -18,7 +18,7 @@ jest.mock('react-native-gesture-handler', () => {
 
 describe('LoginScreen Component', () => {
   it('renders correctly', () => {
-    const { getByPlaceholderText, getByTestId } = render(
+    const { getByPlaceholderText, getByTestId, getByText } = render(
       <AuthProvider>
         <GestureHandlerRootView> {/* Wrap test in GestureHandlerRootView */}
           <NavigationContainer>
@@ -31,5 +31,56 @@ describe('LoginScreen Component', () => {
     expect(getByPlaceholderText('Email')).toBeTruthy();
     expect(getByPlaceholderText('Password')).toBeTruthy();
     expect(getByTestId('LoginButton')).toBeTruthy();
+    expect(getByText("Don't have an account? Sign up")).toBeTruthy();
   });
+
+
+  it('alerts user if fields are not filled in', () => {
+    const { getByText, getByTestId, getByPlaceholderText } = render(
+      <AuthProvider>
+        <GestureHandlerRootView> {/* Wrap test in GestureHandlerRootView */}
+          <NavigationContainer>
+            <LoginScreen />
+          </NavigationContainer>
+        </GestureHandlerRootView>
+      </AuthProvider>
+    );
+
+    let loginButton = getByTestId('LoginButton')
+
+    // both the email and password are blank
+    fireEvent.press(loginButton)
+    expect(getByText("Please fill out both fields")).toBeTruthy();
+    let closeButton = getByText("Close")
+    fireEvent.press(closeButton)
+
+    // no password but an email is entered
+    fireEvent.changeText(getByPlaceholderText('Email'), 'Bob@bob.com');
+    fireEvent.press(loginButton)
+    expect(getByText('Please fill out your password')).toBeTruthy();
+    fireEvent.press(closeButton)
+
+    // no email but a password is entered
+    fireEvent.changeText(getByPlaceholderText('Email'), '');
+    fireEvent.changeText(getByPlaceholderText('Password'), 'password');
+    fireEvent.press(loginButton)
+    expect(getByText('Please fill out your email')).toBeTruthy();
+    fireEvent.press(closeButton)
+
+
+    // email is missing @
+    fireEvent.changeText(getByPlaceholderText('Email'), 'bob.com');
+    fireEvent.changeText(getByPlaceholderText('Password'), 'password');
+    fireEvent.press(loginButton)
+    expect(getByText('Please fill out a valid email')).toBeTruthy();
+    fireEvent.press(closeButton)
+
+    // email is missing .
+    fireEvent.changeText(getByPlaceholderText('Email'), 'bob@bob');
+    fireEvent.changeText(getByPlaceholderText('Password'), 'password');
+    fireEvent.press(loginButton)
+    expect(getByText('Please fill out a valid email')).toBeTruthy();
+    fireEvent.press(closeButton)
+  });
+  
 });
